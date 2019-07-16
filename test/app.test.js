ec3721ec259da0cd28c30525ca9f3df61c905e18
Lili Boxer <ninaliboxer@gmail.com>
2019-07-16 11:08:25 -0700

    testing file set up

--- test/app.test.js
+++ test/app.test.js
@@ -1,1 +1,24 @@
 require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mongoose = require('mongoose');
+
+// connection config
+const connect = require('../lib/utils/connect');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+  
+}); 
