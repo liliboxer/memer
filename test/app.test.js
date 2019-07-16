b9d974dae0657d3c5d2c814bbc9e9cb2453169e6
Lili Boxer <ninaliboxer@gmail.com>
2019-07-16 11:54:32 -0700

    first test path passing

--- test/app.test.js
+++ test/app.test.js
@@ -20,5 +20,23 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
-  
+
+  it('creates meme with POST', () => {
+    return request(app)
+      .post('/api/v1/memes')
+      .send({
+        topField: 'this is a meme',
+        image: 'some URL',
+        bottomField: 'some more text'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          __v: 0,
+          _id: expect.any(String),
+          topField: 'this is a meme',
+          image: 'some URL',
+          bottomField: 'some more text',
+        });
+      });
+  });
 }); 
